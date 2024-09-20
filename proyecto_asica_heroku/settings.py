929863cb08a7ec285a958a6e948606f951979c89
kdev2211 <cristiancariax22@gmail.com>
2024-09-19 21:46:30 -0600

    ss

--- proyecto_asica_heroku/settings.py
+++ proyecto_asica_heroku/settings.py
@@ -160,9 +160,6 @@ django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'global_static'),
-)
 
 
 
@@ -204,21 +201,3 @@ CELERY_TIMEZONE = 'UTC'
 
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'django_errors.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-}
